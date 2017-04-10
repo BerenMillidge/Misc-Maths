184a1cb3a6bdf43e5deabb9a08546cdb39e5ab8e
Beren Millidge <bmillidgework@gmail.com>
2017-04-10 21:36:56 +0100

    split brain auto initial commit

---
+++ split-brain.py
@@ -0,0 +1,9 @@
+#okay, rhis is perhaps where we attempt to start our implementation of the split-brain autoencoder, whic hwe need to work on tomorrow, ebcause today has been disastrous wrt the procrastination compared to the weekend where we did okay! I'll probably actually write the thing in a jupyter notebook instead - as that might be easier, bu t putting it here!
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+#do stuff here
+pass
