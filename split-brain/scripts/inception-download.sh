6c4346e084a1aa3038a5cd95b3ad6d53130f7ed5
Beren Millidge <bmillidgework@gmail.com>
2017-04-16 01:54:04 +0100

    download script

---
+++ split-brain/scripts/inception-download.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# This script downloads the prewritten inception V3 checkpoint for the slim tf.contrib package. I think these sorts of base networks could be very useful for my project as I don't really have days to train convnets, at least not for my initial experiments, so I'll be mostly working on fine-tuning and proof of concept
+
+CHECKPOINT_DIR=/tmp/checkpoints
+mkdir ${CHECKPOINT_DIR}
+wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
+tar -xvf inception_v2_2016_08_28.tar.gz
+mv inception_v3.ckpt ${CHECKPOINT_DIR}
+rm inception_v3_2016_08_28.tar.gz
