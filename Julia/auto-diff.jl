0d868dc8dd74a200c1b471621c8364e79bebd2f3
Beren Millidge <bmillidgework@gmail.com>
2017-12-14 09:41:34 +0000

    debugged AD operators

--- Julia/auto-diff.jl
+++ Julia/auto-diff.jl
@@ -39,98 +39,109 @@ end
 
 
 
-# okay, let's implement our AD functions
+# okay, let's implement our AD functions using overloaded operators
 
 #plus function
-function +*(u::Dual, v::Dual)
+function +(u::Dual, v::Dual)
 	return Dual((u.r + v.r), (u.d+v.d))
 end
 
 #multiply function
-function **(u::Dual, v::Dual)
+function *(u::Dual, v::Dual)
 	return Dual((u.r*v.r), (u.d *v.r + u.r * v.d))
 end
 
-function -*(u::Dual, v::Dual)
+function -(u::Dual, v::Dual)
 	return Dual((u.r-v.r), (u.d-r.d))
 end
 
-function negate*(u::Dual)
+function negate(u::Dual)
 	return Dual(-u.r, -u.d)
 end
 
-function signum*(u::Dual)
+function signum(u::Dual)
 	return Dual(signum(u.r),0)
 end
 
-function abs*(u::Dual)
+function abs(u::Dual)
 	return dual(abs(u.r), u.d*(signum(u.r)))
 end
 
-function /*(u::Dual, v::Dual)
-	return Dual((u.r/v.r), (u.d*v.r - u.r*v.d)/(v.r^2)))
+function /(u::Dual, v::Dual)
+	return Dual((u.r/v.r), ((u.d*v.r - u.r*v.d)/(v.r^2)))
 end
 
 function exp(u::Dual)
-	return Dual(exp(u.r), (u.d*exp(u.r))
+	return Dual(exp(u.r), (u.d*exp(u.r)))
 end
 
-function sqrt*(u::Dual)
-	return Dual(sqrt(u.r), (u.d/(2*sqrt(u.r)))
+function sqrt(u::Dual)
+	return Dual(sqrt(u.r), (u.d/(2*sqrt(u.r))))
 end
 
-function log*(u::Dual)
-	return Dual((log(u.r), (u.r/u.d))
+function log(u::Dual)
+	return Dual(log(u.r), (u.r/u.d))
 end  
 
-function sin*(u::Dual)
-	return Dual(sin(u.r), (u.d*cos(u.r))
+function sin(u::Dual)
+	return Dual(sin(u.r), (u.d*cos(u.r)))
 end
 
-function cos*(u::Dual)
-	return Dual((cos(u.r)), (-u.d*sin(u.r))
+function cos(u::Dual)
+	return Dual(cos(u.r), (-u.d*sin(u.r)))
 end
 
-function tan*(u::Dual)
-	return Dual(tan(u.r), (1/(cos(u.r)^2))
+function tan(u::Dual)
+	return Dual(tan(u.r), (1/(cos(u.r)^2)))
 end
 
-function asin*(u::Dual)
-	return Dual(asin(u.r), (u.d/(sqrt(1-u.r)^2))
+function asin(u::Dual)
+	return Dual(asin(u.r), (u.d/(sqrt(1-u.r)^2)))
 end
 
-function acos*(u::Dual)
+function acos(u::Dual)
 	return Dual(acos(u.r), (-u.d/sqrt(1-u.r)^2))
 end
 
-function atan*(u::Dual)
+function atan(u::Dual)
 	return Dual(atan(u.r), (u.d/(1+u.r)^2))
 end
 
-function sinh*(u::Dual)
-	return Dual(sinh(u.r), (u.d*cosh(u.r))
+function sinh(u::Dual)
+	return Dual(sinh(u.r), (u.d*cosh(u.r)))
 end
 
-function cosh*(u::Dual)
-	return Dual(cosh(u.r), (u.d*sinh(u.r))
+function cosh(u::Dual)
+	return Dual(cosh(u.r), (u.d*sinh(u.r)))
 end
 
-function tanh*(u::Dual)
-	return Dual(tanh(u.r), (u.d * (1- (tanh(u.r))^2)
+function tanh(u::Dual)
+	return Dual(tanh(u.r), (u.d * (1- (tanh(u.r))^2)))
 end
 
-function asinh*(u::Dual)
-	return Dual(asinh(u.r), (u.d/(sqrt(1 + u.r^2)))
+function asinh(u::Dual)
+	return Dual(asinh(u.r), (u.d/(sqrt(1 + u.r^2))))
 end
 
-function acosh*(u::Dual)
+function acosh(u::Dual)
 	return Dual(acosh(u.r), (u.d/sqrt(u.r^2 -1)))
 end
 
-function atanh*(u::Dual)
-	return Dual(atanh(u.r), (u.d/1-u.r^2)))
+function atanh(u::Dual)
+	return Dual(atanh(u.r), (u.d/(1-u.r^2)))
 end
 	
 
+
+# okay, let's test this
+
+function f(a,b)
+	return a + b
+end
+
+a = Dual(5)
+b = Dual(6)
+
+f(a,b)
 	
 
