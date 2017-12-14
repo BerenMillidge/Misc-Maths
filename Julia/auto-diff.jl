8c06b1bdc6ef6a2b893b1d69a71f0d4a42ecbcba
Beren Millidge <bmillidgework@gmail.com>
2017-12-14 09:20:37 +0000

    added finite differences and some more overloaded operators

--- Julia/auto-diff.jl
+++ Julia/auto-diff.jl
@@ -31,6 +31,14 @@ end
 
 
 
+# okay, now we're going to implement finite differences so I can check my answers
+
+function finite_differences(f::Function, start, step)
+	return (f(start+step) - f(step))/step
+end
+
+
+
 # okay, let's implement our AD functions
 
 #plus function
@@ -63,8 +71,8 @@ function /*(u::Dual, v::Dual)
 	return Dual((u.r/v.r), (u.d*v.r - u.r*v.d)/(v.r^2)))
 end
 
-function ^*(u::Dual, v)
-	return Dual((u.r^v), (u.d*(u.r^v)))
+function exp(u::Dual)
+	return Dual(exp(u.r), (u.d*exp(u.r))
 end
 
 function sqrt*(u::Dual)
@@ -87,8 +95,42 @@ function tan*(u::Dual)
 	return Dual(tan(u.r), (1/(cos(u.r)^2))
 end
 
-function asin*(u::Dual
+function asin*(u::Dual)
+	return Dual(asin(u.r), (u.d/(sqrt(1-u.r)^2))
+end
+
+function acos*(u::Dual)
+	return Dual(acos(u.r), (-u.d/sqrt(1-u.r)^2))
+end
+
+function atan*(u::Dual)
+	return Dual(atan(u.r), (u.d/(1+u.r)^2))
+end
+
+function sinh*(u::Dual)
+	return Dual(sinh(u.r), (u.d*cosh(u.r))
+end
+
+function cosh*(u::Dual)
+	return Dual(cosh(u.r), (u.d*sinh(u.r))
+end
+
+function tanh*(u::Dual)
+	return Dual(tanh(u.r), (u.d * (1- (tanh(u.r))^2)
+end
+
+function asinh*(u::Dual)
+	return Dual(asinh(u.r), (u.d/(sqrt(1 + u.r^2)))
+end
+
+function acosh*(u::Dual)
+	return Dual(acosh(u.r), (u.d/sqrt(u.r^2 -1)))
+end
+
+function atanh*(u::Dual)
+	return Dual(atanh(u.r), (u.d/1-u.r^2)))
+end
 	
 
-# okay, now we're going to implement finite differences so I can check my answers
+	
 
