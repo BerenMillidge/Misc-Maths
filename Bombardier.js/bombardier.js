7b4a1c7381ba55785f6bdeaa893b55ea9d4f8218
Beren Millidge <bmillidgework@gmail.com>
2018-01-03 21:56:13 +0000

    Add files via upload

---
+++ Bombardier.js/bombardier.js
@@ -0,0 +1,24 @@
+// so the aim of this is just a functoin that takes other functions and "bombards" them 
+// with all kind of inputs to form a useful testing program ratherthan anything
+// kind of silly to be honest like anything else
+// I don't even know to be honest
+// like the opint of testing isn't really to confirm the path works, but for edge cases
+// where this is difficult
+// so I don't even know
+
+// okay, our big functoin, I honestly do not even know how this won't work in js
+// like there are probably loads of useful javascript specific things to do, I don't know
+
+
+
+const standard_val = {
+
+	// this is just our huge javascript object which will be our standard value hers
+	// of numbers and stuff, I don't know
+}
+
+function bombard!(fn, num_args, user_input_args, standard_vals = standard_val) {
+
+
+		
+}
