6d71503218b3db6e4d66c40ff0f720ea0e0fd59e
Beren Millidge <bmillidgework@gmail.com>
2018-01-05 15:34:49 +0000

    Add files via upload

---
+++ BlockchainJSTutorial/server.ts
@@ -0,0 +1,44 @@
+// we needto control our node. this is done by setting up a http server
+// so let's do this now
+
+// we set up a very simple express server
+
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+
+import {Block, generateNextBlock, getBlockchain} from './blockchain';
+import {connectToPeers, getSockets, initP2PServer} from '/p2p';
+
+const port: number = parseInt(process.env.HTTP_PORT) || 3001;
+const p2pPort: number = parseInt(process.env.P2P_PORT) || 6001;
+
+const initHttpServer = function(myPort: number) {
+	const app = express();
+
+	app.use(bodyParser.json());
+
+	app.get('/blocks', function(req, res) {
+		res.send(getBlockchain())
+	});
+
+	app.post('/mineBlock', function(req, res) {
+		const newBlock: Block = generateNextBlock(req.body.data);
+		res.send(newBlock);
+	});
+	app.get('/peers', (req, res) => {
+        res.send(getSockets().map(( s: any ) => s._socket.remoteAddress + ':' + s._socket.remotePort));
+});
+	app.post('/addPeer', function(req,res) {
+		connectToPeers(req.body.peer);
+		res.send();
+	});
+
+	app.listen(myPort, function() {
+		console.log('listeneing http on port' + myPort);
+	});
+};
+
+initHttpServer(port);
+initP2PServer(p2pPort);
+
+
