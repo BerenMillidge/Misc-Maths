03a6d81510b94f8e14684f189145d55af5370c0f
Beren Millidge <bmillidgework@gmail.com>
2018-01-06 16:43:15 +0000

    added transaction support

--- BlockchainJSTutorial/transaction.ts
+++ BlockchainJSTutorial/transaction.ts
@@ -124,6 +124,122 @@ const signTxIn = function(transaction: Transaction, txInIndex: number,
 // transactoin outputs since we're spending things and shuffling it around
 // so first we need to get the transactoins, then see what are consumed and the update the resulting
 
+// we also now need to lay out our transaction validatoin rules, which isn't so bad or difficult
+// or horrendous generally, but it could be
+
+const isValidTransactionStructure = function(transaction:Transaction) {
+	if (typeof transaction.id !== 'string') {
+		console.log('invalid transaction id');
+		return false;
+	}
+	if (typeof transaction.txIns !==TxIn[]) {
+		console.log('invalid transaction inputs');
+		return false;
+	}
+	if (typeof transaction.txOuts !== TxOut[]) {
+		console.log('invalid transaction outputs');
+		return false;
+	}
+	if (!transaction.txIns
+            .map(isValidTxInStructure)
+            .reduce((a, b) => (a && b), true)) {
+        return false;
+}
+
+		//we also need the transaction id to be valid
+
+
+	// we also need the signatures to be valid
+}
+
+
+
+// and the general for multiple, using functional programming style instead of proper standard loop
+// which makes way more sense, but probably not to mathematicians
+
+const isValidTransactionsStructure = (transactions: Transaction[]): boolean => {
+    return transactions
+        .map(isValidTransactionStructure)
+        .reduce((a, b) => (a && b), true);
+};
+
+// we need to be ale to sign input transactions which couldcause issues!
+
+const hasDuplicates = function(txIns: TxIn[]): boolean {
+	const groups = _.countBy(txIns, function(txIn) {
+		return txIn.txOutId + txIn.txOutId:
+	});
+	return groups.map(function(value, key) {
+		if(value > 1) {
+			console.log('duplicate txIn: ' + key);
+			return true;
+		} else {
+			return false;
+		}
+	})
+	.includes(true);
+	}
+
+// this gets a transaction given a string orsomething. surely we need the thing
+// or it I honestly don't know, shouldn't thie be querying the blockchain???
+// who even knows?
+
+const getCoinbaseTransaction = function(address: string, blockIndex: number): Transaction {
+	const t = new Transaction();
+	const txIn: TxIn = new TxIn();
+	txIn.signature = "";
+	txIn.txOutId = "";
+	txIn.txOutIndex = blockIndex;
+
+	t.txIns = [txIn];
+	t.txOuts = [new TxOut(address, 'COINBASE_AMOUNT')];
+	t.id = getTransactionId(t);
+	return t;
+}
+
+
+const validateTxIn = function(txIn: TxIn, transaction: Transaction, aUnspentTxOuts: UnspentTxOut[]): boolean {
+	const referencedUTxOut: UnspentTxOut = aUnspentTxOuts.find(function(uTxO) {
+		return UTxO.txOutId === txIn.txOutId && uTxO.txOut ===txIn.txOutId;
+	});
+}
+
+const isValidAddress = function(address: string): boolean {
+	const address_regexp = '^[a-fA-F0-9]+$';
+	if(address.length !==130) {
+		console.log('invalid public ket length');
+		return false;
+	}
+	else if (address.match(address_regexp) === null) {
+		console.log('public key fails regex match');
+		return false;
+	} else if (!address.startsWith('04')) {
+		console.log('public key must start with 04');
+		return false;
+	}
+	return true;
+}
+
+const tohexString = function(byteArray): string {
+	return Array.from(byteArray, function(byte:any) {
+		return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+	}).join('');
+}
+
+const processTransactions = function(transactions: Transaction[], aUnspentTxOuts: UnspentTxOut[], blockIndex: number) {
+	if(!isValidTransactionsStructure(transactions)) {
+		return null;
+	}
+	if(!validateBlockTransactions(transactions, aUnspentTxOuts, blockIndex)) {
+		console.log('invalid block transactions');
+		return null;
+	}
+	return updateUnspentTxOuts(transactions, aUnspentTxOuts);
+
+}
+const getPublicKey = function(aPriveyKey: string): string {
+	return ec.keyFromPrivate(aPrivateKey, 'hex').getPublic().encode('hex');
+}
 
 const updateUnspentTxOuts = function(newTransactions: Transaction[], aUnspentTxOuts: UnspentTxOut[]): UnspentTxOut[] {
 	const newUnspentTxOuts: UnspentTxOut[] = newTransactions
@@ -149,3 +265,11 @@ const updateUnspentTxOuts = function(newTransactions: Transaction[], aUnspentTxO
 		.concat(newUnspentTxOuts);
 		return resultingUnspentTxOuts;
 }
+
+
+
+
+
+
+
+
