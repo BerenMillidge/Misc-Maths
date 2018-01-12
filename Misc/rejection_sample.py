9213bc177042d68ca8b7ab2dc6fdfddf88b06b52
Beren Millidge <bmillidgework@gmail.com>
2018-01-12 17:37:58 +0000

    a tiny bit more. I mean I think this algorithm is completely wrong so far... but who knows?

--- Misc/rejection_sample.py
+++ Misc/rejection_sample.py
@@ -44,14 +44,25 @@ def finite_difference(fn, point, peturbation = 1e-6):
 
 # okay, now let's get our points of abscissae
 abscissae = []
+hs = []
 abscissae_derivatives = []
 
-def calculate_abscissae_derivatives(abscissae, fn):
+def get_hs(abscissae, fn):
+	hs =[]
+	for i in xrange(abscissae):
+		hs.append(fn(abscissae[i]))
+	hs = np.array(hs)
+	return hs
+
+def calculate_abscissae_derivatives_and_hs(abscissae, fn):
+	hs= []
 	abscissae_derivatives = []
 	for i in xrange(len(abscissae)):
+		hs.append(fn(abscissae[i]))
 		abscissae_derivatives.append(finite_difference(fn, abscissae[i]))
 	abscissae_derivatives = np.array(abscissae_derivatives)
-	return abscissae_derivatives
+	hs = np.array(hs)
+	return abscissae_derivatives, hs
 
 		
 def get_tangent_intersection_points(abscissae, abscissae_derivatives):
@@ -68,4 +79,52 @@ def get_tangent_intersection_points(abscissae, abscissae_derivatives):
 	zs = np.array(zs)
 	return zs
 
+# I'm really not sure what datastructures we're going to use to figure this out. first let's actually try to sample a point and see what's going on
+
+def get_piece_cumsum(hx, hdashx, z, zminus,x):
+	int1 = np.exp((hx - x*hdashx)/hdashx))
+	int2 = np.exp(z*hdashx) - np.exp(zminus*hdashx)
+	return int1*int2
+
+def get_cumsums(abscissae, abscissae_derivatives, zs,hs):
+	cumsums = []
+	total = 0
+	# I think there should be one less z than xs so that's good, so lets assert that and then ope for the bes
+	assert len(abscissae) == len(abscissae_derivatives) == len(hs) == len(zs) +1, 'lengths are incorrect'
+	for i in xrange(len(zs)):
+		if i == 0:
+			cumsum= get_piece_cumsum(hs[i], abscissae_derivatives[i], zs[i], 0,abscissae[i])
+			cumsums.append(cumsum)
+			total += cumsum
+		cumsum= get_piece_cumsum(hs[i], abscissae_derivatives[i],zs[i], zs[i-1], abscissae[i])
+		cumsums.append(cumsum)
+		total +=cumsum
+	cumsums = np.array(cumsums)
+	return cumsums, total
+	
+
+def get_index_upper(cumsums, total):
+	curr = 0
+	for i in xrange(len(cumsums)):
+		curr += cumsums[i]
+		if total <= curr:
+			diff = curr - total
+			return i,diff
+	#I don't think we should need this, as it should always return in the loop, I think
+	#if it goes through all of them
+	raise ValueError('total is greater than cumulative sum!')
+	#return len(cumsum)
+
+def sample_single_piece(cum, hdashx, hx,x):
+	frac = (cum *hdashx)/(np.exp(hdashx) - (x*hdashx))
+	return (1/hdashx)*np.log((np.exp(hdashx) + frac))
+
+def sample_upper(xs, hs, hdashes,zs):
+	u = np.random.uniform(0,1)
+	#we ned cumulative sum now
+	cumsums, total = get_cumsums(xs, hdashes, zs, hs)
+	c = u*total
+	#now we need to get the index of the cumulative sum so we can calculate the region we need it in
+	i,diff = get_index_upper(cumsums, c)
+	sample = sample_single_piece(diff, hdashes[i],hs[i],xs[i])	
 
